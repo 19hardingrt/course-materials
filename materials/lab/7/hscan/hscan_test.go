2df8630edd9e6d6d5068f2de236c86df3d5376bf
Ryan Harding <70543859+19hardingrt@users.noreply.github.com>
2022-05-06 23:12:53 -0700

    Update hscan_test.go

--- materials/lab/7/hscan/hscan_test.go
+++ materials/lab/7/hscan/hscan_test.go
@@ -21,12 +21,12 @@ func TestGenHashMaps(t *testing.T) {
 
 func DrBPassword(t *testing.T) {
 	GenHashMaps("../main/gmail-alleged.txt")
-	got1, _ := GetMD5("90f2c9c53f66540e67349e0ab83d8cd0")
-	want1 := "p@ssword"
-	got2, _ := GetSHA("1c8bfe8f801d79745c4631d09fff36c82aa37fc4cce4fc946683d7b336b63032")
-	want2 := "letmein"
-	if got1 != want1 || got2 != want2 {
-		t.Errorf("got1 %s, wanted1 %s\ngot2 %s, wanted2 %s", got1, want1, got2, want2)
+	gotMD5, _ := GetMD5("90f2c9c53f66540e67349e0ab83d8cd0")
+	wantMD5 := "p@ssword"
+	gotSHA, _ := GetSHA("1c8bfe8f801d79745c4631d09fff36c82aa37fc4cce4fc946683d7b336b63032")
+	wantSHA := "letmein"
+	if gotMD5 != wantMD5 || gotSHA != wantSHA {
+		t.Errorf("got1 %s, wanted1 %s\ngot2 %s, wanted2 %s", gotMD5, wantMD5, gotSHA, wantSHA)
 	}
 
 }
